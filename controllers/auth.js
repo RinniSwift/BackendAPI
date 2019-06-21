1a7f98e95f89b34bfb486ab84a20ba42c3e35692
Rinni Swift <rinradaswift@gmail.com>
2019-06-20 20:35:38 -0700

    update

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,27 +2,21 @@ const jwt = require('jsonwebtoken');
 const User = require('../models/user');
 
 module.exports = app => {
-
     // GET route to signup page
     app.get('/sign-up', function(req, res){
-        res.status(200).render("sign-up")
+      res.status(200).render("sign-up")
     })
 
     // POST route
     app.post('/sign-up', (req, res) => {
         const newUser = new User(req.body);
-
         newUser.save()
           .then( (savedUser) => {
               const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-              res.json({ token });
-
               console.log("signed token in jwt");
+              res.json({ token });
           }).catch( (error) => {
-            response.status(400).json({ "error" : error})
+            res.status(400).json({ "error" : error })
           })
     })
-
-
-    
 };
