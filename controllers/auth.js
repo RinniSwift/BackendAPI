faf35daca2aa5f17f25758c213b89cf87602df99
Rinni Swift <rinradaswift@gmail.com>
2019-06-21 17:41:08 -0700

    added helper routes for all services and auth route

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,22 +1,32 @@
+// - controllers/auth.js
+
+const express = require('express'),
+      router = express.Router();
+
 const jwt = require('jsonwebtoken');
+
 const User = require('../models/user');
 
-module.exports = app => {
-    // GET route to signup page
-    app.get('/sign-up', function(req, res){
-      res.status(200).render("sign-up")
-    })
 
-    // POST route
-    app.post('/sign-up', (req, res) => {
-        const newUser = new User(req.body);
-        newUser.save()
-          .then( (savedUser) => {
-              const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-              console.log("signed token in jwt");
-              res.json({ token });
-          }).catch( (error) => {
-            res.status(400).json({ "error" : error })
-          })
-    })
-};
+// - Routes 
+
+router.get('/',(req,res) => {
+  console.log("in here")
+  res.status(200).render('sign-up')
+})
+
+router.post('/',(req,res) => {
+  console.log("in here")
+  const newUser = new User(req.body);
+  newUser.save()
+  .then( (savedUser) => {
+      const token = jwt.sign({ _id: user._id }, process.env.SECRET);
+      console.log("signed token in jwt");
+      res.json({ token });
+  }).catch( (error) => {
+    res.status(400).json({ "error" : error })
+  })
+})
+
+
+module.exports = router;
