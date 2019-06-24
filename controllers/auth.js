9db879873de622efa80db3eee8d23321a069bd12
SarinSwift <sarinyaswift@gmail.com>
2019-06-24 12:36:40 -0700

    signup working

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,28 +1,32 @@
-const jwt = require('jsonwebtoken');
+// - controllers/auth.js
+
+const express = require('express'),
+      jwt = require('jsonwebtoken'),
+      router = express.Router();
+
 const User = require('../models/user');
 
-module.exports = app => {
+// - Routes
 
-    // GET route to signup page
-    app.get('/sign-up', function(req, res){
-        res.status(200).render("sign-up")
-    })
+router.get('/',(req,res) => {
+  res.status(200).render('sign-up')
+  console.log("in GET method of route sign-up")
+})
 
-    // POST route
-    app.post('/sign-up', (req, res) => {
-        const newUser = new User(req.body);
+router.post('/',(req,res) => {
+  console.log("in POST method of route sign-up")
+  const newUser = new User(req.body);
+  newUser.save()
+  .then( (savedUser) => {
+      const token = jwt.sign({ _id: savedUser._id }, process.env.SECRET);
+      console.log("signed token in jwt");
+      res.json({ token });
+  }).catch( (error) => {
+    console.log("cant create new User")
+    console.log(req.body)
+    res.status(400).json({ "error" : error })
+  })
+})
 
-        newUser.save()
-          .then( (savedUser) => {
-              const token = jwt.sign({ _id: user._id }, process.env.SECRET);
-              res.json({ token });
 
-              console.log("signed token in jwt");
-          }).catch( (error) => {
-            response.status(400).json({ "error" : error})
-          })
-    })
-
-
-    
-};
+module.exports = router;
