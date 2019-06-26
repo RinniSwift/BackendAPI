b64678de031bdba8ca5e9377cf9a86fffaf802be
Rinni Swift <rinradaswift@gmail.com>
2019-06-26 12:35:15 -0700

    fixed code error on schema

--- models/macroCategory.js
+++ models/macroCategory.js
@@ -1,6 +1,10 @@
 // models/macroCategory
 
 
+const mongoose = require('mongoose'),
+	  Schema = mongoose.Schema;
+
+
 const MacroCategory = new Schema({
 	name: { type: String },
 	micro_categories: [{ type: Schema.Types.ObjectId, ref: "MicroCategory" }]
